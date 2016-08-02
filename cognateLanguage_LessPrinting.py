ebd186f563131484791887e5ae97226848b5cfe4
hchiam <hchiam@gmail.com>
2016-08-01 22:04:19 -0400

    fix to putting original consonants back in, and created version of code with less prints

--- cognateLanguage_LessPrinting.py
+++ cognateLanguage_LessPrinting.py
@@ -78,33 +78,21 @@ for line in data:
 
 originalWords = words.copy() # must explicitly make copy of dictionary in Python (instead of a reference)
 
-# show the words:
-print '\nshow the words:'
-
-for language in words:
-    if language != 'Eng':
-        print language + '\t' + words[language]
-
-# show the words spelt with just the initial vowel and consonants:
-print '\nshow the words spelt with just the initial vowel and consonants:'
+# the words spelt with just the initial vowel and consonants:
 
 for language in words:
     if language != 'Eng':
         words[language] = respellWithInitialVowelAndConsonants(words[language])
-        print language + '\t' + words[language]
 
 wordsMinusNoninitialVowels = words.copy() # must explicitly make copy of dictionary in Python (instead of a reference)
 
-# show the words with allophonic spelling:
-print '\nshow the words with allophonic spelling:'
+# the words with allophonic spelling:
 
 for language in words:
     if language != 'Eng':
         words[language] = respellWithAllophones(words[language])
-        print language + '\t' + words[language]
 
 # ignore repeats:
-print '\nignore repeats:'
 
 shortList = words
 
@@ -114,12 +102,7 @@ for language in shortList:
             if shortList[language] == shortList[otherlanguage]:
                 shortList[otherlanguage] = ''
 
-for language in shortList:
-    if language != 'Eng':
-        print language + '\t' + shortList[language] # print the unique words
-
 # ignore words embedded in other words:
-print '\nignore words embedded in other words:'
 
 for language in shortList:
     for otherlanguage in shortList:
@@ -127,56 +110,32 @@ for language in shortList:
             if shortList[language] in shortList[otherlanguage]:
                 shortList[language] = ''
 
-for language in shortList:
-    if language != 'Eng':
-        print language + '\t' + shortList[language]
-
 # find overlaps in words:
 
 for tries in range(5):
-    print '\nfind overlaps in words:  try #' + str(tries+1)
 
     shortList = combineOverlappingWords(shortList)
 
-    for language in shortList:
-        if language != 'Eng':
-            print language + '\t' + shortList[language]
-
 # append remaining words:
-print '\nappend remaining words:'
 
 for language in shortList:
     if language != 'Eng':
         newWord += shortList[language]
 
-print newWord
 
 # put original consonants back in, using 1st letters of higher-priority words:
-print '\nput original consonants back in, using 1st letters of higher-priority words:'
-#print
-#print 'originalWords = \n', originalWords, '\n'
-#print 'wordsMinusNoninitialVowels = \n', wordsMinusNoninitialVowels, '\n'
-#print 'words = \n', words, '\n'
-print words
 for language in reversed(words.keys()):
     if language != 'Eng':
-        print wordsMinusNoninitialVowels[language]
         patternCompressedAllo = respellWithAllophones(wordsMinusNoninitialVowels[language])
         if wordsMinusNoninitialVowels[language] not in newWord:
             if patternCompressedAllo in respellWithAllophones(newWord): # replace with compressed word in allophone form
                 index = respellWithAllophones(newWord).find(patternCompressedAllo)
                 newWord = newWord[:index] + wordsMinusNoninitialVowels[language] + newWord[index+len(patternCompressedAllo):]
-                print ':', patternCompressedAllo
             else:
                 pattern = respellWithAllophones(wordsMinusNoninitialVowels[language])
                 newWord = newWord.replace(pattern, wordsMinusNoninitialVowels[language])
-            print ' ->\t', newWord
-
-print "\nnewWord = ", newWord
 
 # put original vowels (& consonants) back in, favouring higher-priority words:
-print '\nput original vowels (& consonants) back in, favouring higher-priority words:'
-print originalWords
 for language in reversed(words.keys()):
     if language != 'Eng':
         if originalWords[language] not in newWord:
@@ -188,22 +147,12 @@ for language in reversed(words.keys()):
             tempWord = newWord
             # check if allophones exist that can be replaced, before checking for replacing compressed original word:
             if patternOrigAllo in respellWithAllophones(newWord):
-                print 'replace patternOrigAllo:'
                 index = respellWithAllophones(newWord).find(patternOrigAllo)
                 newWord = newWord[:index] + replacer + newWord[index+len(patternOrigAllo):]
             elif patternCompressedAllo in respellWithAllophones(newWord):
-                print 'replace patternCompressedAllo:'
                 index = respellWithAllophones(newWord).find(patternCompressedAllo)
                 newWord = newWord[:index] + replacer + newWord[index+len(patternCompressedAllo):]
             else:
-                print 'replace patternOrigCompressed:', patternOrigCompressed
                 newWord = newWord.replace(patternOrigCompressed, replacer,1)
-            print '\t', replacer, '\t->\t', newWord, '<changed>'
-
-print "\nnewWord = ", newWord
-print "\noriginal words = "
-for language in words:
-    if language != 'Eng':
-        print language + '\t' + originalWords[language]
 
 
