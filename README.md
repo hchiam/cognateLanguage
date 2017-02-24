608984f696450a7000eeac1d00b246cc6798c177
hchiam <hchiam@gmail.com>
2017-02-24 16:00:21 -0500

    add trinket link and repeatTranslation.py

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 **Short description**:  My "coglang" project.  Possibly a "language" to help you learn multiple languages at the same time (but only [receptive vocab](https://en.wikipedia.org/wiki/Vocabulary#Productive_and_receptive)).  Technically, it's a functional conlang consisting of only vocabulary.
 
+Just want to test out some translations?  Go to [this website](https://trinket.io/python3/013b0a586a?outputOnly=true&runOption=run) to run the code in your browser:  [https://trinket.io/python3/013b0a586a?outputOnly=true&runOption=run](https://trinket.io/python3/013b0a586a?outputOnly=true&runOption=run)
+
 **Prereq**:  Effective mnemonics, i.e. memory aids (see the ones on Memrise for the kinds described under [http://www.memrise.com/science/](http://www.memrise.com/science/)).  I'm not talking about things like acrostics and first-letter memory aids.  It might help to google "[Benny Lewis imagination](https://www.fluentin3months.com/imagination-your-key-to-memorizing-hundreds-of-words-quickly/)" and "[Ron White mind palace](https://www.youtube.com/watch?v=3vlpQHJ09do)".  These are the kinds of memory techniques also described in Barbara Oakley's "A Mind for Numbers" and Cal Newport's "Deep Work".
 
 **Example 1**:  "you" = *"[entuni](https://drive.google.com/open?id=0B239lCkYOdXfbHpwZjZfamNoelk)"*, which is a single word that embeds these words: 
---
+++ repeatTranslation.py
@@ -0,0 +1,121 @@
+import sys
+    
+data = []
+
+# use different import based on python version number:
+if (sys.version_info > (3, 0)):
+    # python 3:
+    import urllib.request
+    with urllib.request.urlopen('https://raw.githubusercontent.com/hchiam/cognateLanguage/master/output_shortlist.txt') as response:
+        line = response.readline().decode('utf-8').replace('\n','')
+        while line != '':
+            data.append(line)
+            line = response.readline().decode('utf-8').replace('\n','')
+else: 
+    # python 2:
+    import urllib2
+    response = urllib2.urlopen('https://raw.githubusercontent.com/hchiam/cognateLanguage/master/output_shortlist.txt')
+    data = response.read().split('\n')
+
+import string # example use here:  to be able to remove punctuation
+import re # example use here:  to be able to remove repeating spaces
+
+keepGoing = True
+
+while keepGoing:
+    
+    def justTwoInitSylls(word):
+        beforeThisIndex = 0
+        for vowel1 in word:
+            if vowel1 in 'aeiou':
+                afterThisIndex = word.index(vowel1)
+                break
+        for vowel2 in word[afterThisIndex+1:]:
+            if vowel2 in 'aeiou':
+                beforeThisIndex = word[afterThisIndex+1:].index(vowel2)+1 + afterThisIndex+1
+                break
+        if beforeThisIndex!=0:
+            word = word[:beforeThisIndex+1]
+        return word
+    
+    def countVowels(word):
+        vowels = 'aeiou'
+        word = word.lower()
+        count = 0
+        for char in word:
+            if char in vowels:
+                count += 1
+        return count
+    
+    # filename = 'output_shortlist.txt'
+    # data = ''
+    # input = ''
+    translation = '< Translation Not Found. >'
+    
+    if (sys.version_info > (3, 0)):
+        inputData = input('Enter English word or sentence gloss to translate:\n\t')
+    else:
+        inputData = raw_input('Enter English word or sentence gloss to translate:\n\t')
+    
+    # remove punctuation from inputData, except for '?'
+    exclude = set(string.punctuation)
+    inputData = ''.join(ch for ch in inputData if (ch not in exclude or ch == '?'))
+    
+    # add space before '?' to enable replacing with question particle word
+    inputData = inputData.replace('?',' ?')
+    
+    # make inputData all lowercase
+    inputData = inputData.lower()
+    
+    # remove repeating spaces from what remains
+    inputData = re.sub(' +', ' ', inputData)
+    
+    # print inputData # debug output
+    
+    if inputData != "":
+        # split inputData into words
+        inputData = inputData.split(' ')
+        
+        translation = ''
+        shortTranslation = ''
+        trackLastLetterOfLastWord = ''
+        
+        for word in inputData:
+            
+            translationFound = False
+            
+            # search for word translation in list
+            for line in data:
+                if line != '\n' and ',' in line:
+                    if word == line.split(',')[1]:
+                        translatedWord = line.split(',')[0]
+                        shortTranslatedWord = justTwoInitSylls(translatedWord)
+                        trackLastLetterOfLastWord = shortTranslatedWord[-1]
+                        numVowelsInTranslatedWord = countVowels(translatedWord)
+                        if numVowelsInTranslatedWord == 1:
+                            shortTranslation += translatedWord
+                            trackLastLetterOfLastWord = ''
+                        elif trackLastLetterOfLastWord in 'aeiou':
+                            shortTranslation += shortTranslatedWord
+                        else:
+                            shortTranslation += shortTranslatedWord[:-1]
+                        translation += translatedWord + ' '
+                        translationFound = True
+            
+            # add in '?' for words not found
+            if translationFound == False:
+                translation += '[?]' + ' '
+    
+        # remove final space ' '
+        translation = translation[:-1]
+        # add final letter
+        shortTranslation += trackLastLetterOfLastWord
+    
+    print ('Long Translation:\n\t' + '"' + translation.capitalize()+'.' + '"')
+    print ('Short Translation:\n\t' + shortTranslation)
+    
+    if (sys.version_info > (3, 0)):
+        userResponse = input('Another sentence? (y/n):\n\t').lower()
+    else:
+        userResponse = raw_input('Another sentence? (y/n):\n\t').lower()
+    keepGoing = ('y' == userResponse) or ('yes' == userResponse)
