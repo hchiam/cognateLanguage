9a98b424595ff7df1dcc45125a7e78601ee209d9
hchiam <hchiam@gmail.com>
2017-03-16 22:11:30 -0400

    use import for modularity

--- repeatTranslation.py
+++ repeatTranslation.py
@@ -1,21 +1,6 @@
-import sys
-    
-data = []
-
-# use different import based on python version number:
-if (sys.version_info > (3, 0)):
-    # python 3:
-    import urllib.request
-    with urllib.request.urlopen('https://raw.githubusercontent.com/hchiam/cognateLanguage/master/output_shortlist.txt') as response:
-        line = response.readline().decode('utf-8').replace('\n','')
-        while line != '':
-            data.append(line)
-            line = response.readline().decode('utf-8').replace('\n','')
-else: 
-    # python 2:
-    import urllib2
-    response = urllib2.urlopen('https://raw.githubusercontent.com/hchiam/cognateLanguage/master/output_shortlist.txt')
-    data = response.read().split('\n')
+from getDataRemotely import run # getDataRemotely.py...
+import sys # ...but still need this...
+data = run() # ...and this.
 
 import string # example use here:  to be able to remove punctuation
 import re # example use here:  to be able to remove repeating spaces
