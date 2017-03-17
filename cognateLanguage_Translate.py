50bb2270c72d159e494916604cfc15f81463782d
hchiam <hchiam@gmail.com>
2017-03-16 23:29:17 -0400

    data = hashtable dictionary instead of long list

--- cognateLanguage_Translate.py
+++ cognateLanguage_Translate.py
@@ -1,6 +1,8 @@
 import string # example use here:  to be able to remove punctuation
 import re # example use here:  to be able to remove repeating spaces
 
+from dictAsFile_wrapper import *
+
 def justTwoInitSylls(word):
     beforeThisIndex = 0
     for vowel1 in word:
@@ -24,8 +26,8 @@ def countVowels(word):
             count += 1
     return count
 
-filename = 'output_shortlist.txt'
-data = ''
+filename = 'hashtable.pkl' # 'output_shortlist.txt'
+data = {}
 input = ''
 translation = '< Translation Not Found. >'
 
@@ -54,32 +56,35 @@ if input != "":
     shortTranslation = ''
     trackLastLetterOfLastWord = ''
     
-    # get lines of file into a list
-    with open(filename,'r') as f:
-        data = f.readlines()
+    # # get lines of file into a list
+    # with open(filename,'r') as f:
+    #     data = f.readlines()
+    
+    # get hashtable file into a dictionary
+    data = readFileToDict(filename)
     
     for word in input:
         
         translationFound = False
         
-        # search for word translation in list
-        for line in data:
-            if line != '\n' and ',' in line:
-                if word == line.split(',')[1]:
-                    translatedWord = line.split(',')[0]
-                    shortTranslatedWord = justTwoInitSylls(translatedWord)
-                    trackLastLetterOfLastWord = shortTranslatedWord[-1]
-                    numVowelsInTranslatedWord = countVowels(translatedWord)
-                    if numVowelsInTranslatedWord == 1:
-                        shortTranslation += translatedWord
-                        trackLastLetterOfLastWord = ''
-                    elif trackLastLetterOfLastWord in 'aeiou':
-                        shortTranslation += shortTranslatedWord
-                    else:
-                        shortTranslation += shortTranslatedWord[:-1]
-                    translation += translatedWord + ' '
-                    translationFound = True
+        # # search for word translation in list
         
+        # search for word translation in data ("data" is a hashtable/dictionary)
+        if word in data:
+            translatedWord = data[word]
+            shortTranslatedWord = justTwoInitSylls(translatedWord)
+            trackLastLetterOfLastWord = shortTranslatedWord[-1]
+            numVowelsInTranslatedWord = countVowels(translatedWord)
+            if numVowelsInTranslatedWord == 1:
+                shortTranslation += translatedWord
+                trackLastLetterOfLastWord = ''
+            elif trackLastLetterOfLastWord in 'aeiou':
+                shortTranslation += shortTranslatedWord
+            else:
+                shortTranslation += shortTranslatedWord[:-1]
+            translation += translatedWord + ' '
+            translationFound = True
+            
         # add in '?' for words not found
         if translationFound == False:
             translation += '[?]' + ' '
