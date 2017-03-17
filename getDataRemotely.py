65e76f01053b724cb49d1d85623c715f653b86c6
hchiam <hchiam@gmail.com>
2017-03-16 22:10:44 -0400

    make modluar for import

--- getDataRemotely.py
+++ getDataRemotely.py
@@ -1,21 +1,26 @@
 import sys
+
+def run():    
+    data = []
     
-data = []
-
-# use different import based on python version number:
-if (sys.version_info > (3, 0)):
-    # python 3:
-    import urllib.request
-    with urllib.request.urlopen('https://raw.githubusercontent.com/hchiam/cognateLanguage/master/output_shortlist.txt') as response:
-        line = response.readline().decode('utf-8').replace('\n','')
-        while line != '':
-            data.append(line)
+    # use different import based on python version number:
+    if (sys.version_info > (3, 0)):
+        # python 3:
+        import urllib.request
+        with urllib.request.urlopen('https://raw.githubusercontent.com/hchiam/cognateLanguage/master/output_shortlist.txt') as response:
             line = response.readline().decode('utf-8').replace('\n','')
-else: 
-    # python 2:
-    import urllib2
-    response = urllib2.urlopen('https://raw.githubusercontent.com/hchiam/cognateLanguage/master/output_shortlist.txt')
-    data = response.read().split('\n')
+            while line != '':
+                data.append(line)
+                line = response.readline().decode('utf-8').replace('\n','')
+    else: 
+        # python 2:
+        import urllib2
+        response = urllib2.urlopen('https://raw.githubusercontent.com/hchiam/cognateLanguage/master/output_shortlist.txt')
+        data = response.read().split('\n')
+    return data
 
-# debug print out:
-print ('test: data[0] = ' + data[0])
+# this if statement is so that the following code only runs if this .py file is not being imported
+if __name__ == '__main__':
+    data = run()
+    # debug print out:
+    print ('test: data[0] = ' + data[0])
