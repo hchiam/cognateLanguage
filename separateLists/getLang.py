15a20f6ed391775692a5974443650936c0963fbf
hchiam <hchiam@gmail.com>
2017-03-28 21:20:44 -0400

    add separate lists generated

---
+++ separateLists/getLang.py
@@ -0,0 +1,13 @@
+data = []
+
+inputFile = '../output_shortlist.txt' # '../' makes it look at the folder directory one above for that file
+with open(inputFile,'r') as f1:
+    data = f1.readlines()
+
+langs = ['Chi','Spa','Hin','Ara','Rus']
+for i,lang in enumerate(langs):
+    outputFile = 'just' + lang + '.txt'
+    for line in data:
+        entry = line.split(',')
+        with open(outputFile,'a') as f2:
+            f2.write(entry[i+2] + ',' + entry[1] + '\n') # i+2 because item 0=CogLang and 1=Eng
