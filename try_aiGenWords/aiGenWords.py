d9c36e2cf35d821e475b38bfc496dffb1f900186
hchiam <hchiam@gmail.com>
2017-04-24 01:03:26 -0400

    randomly gets letters from each word (in order)

--- try_aiGenWords/aiGenWords.py
+++ try_aiGenWords/aiGenWords.py
@@ -4,6 +4,10 @@ from random import randint
 inputFile = 'data.txt'
 data = []
 outputFile = 'output.txt'
+newWords = []
+output = []
+
+
 
 def getData():
     with open(inputFile,'r') as f:
@@ -17,22 +21,31 @@ def generateWord(entry):
     outputWord = ''
     entry = entry.split(',')
     srcWords = entry[2:7]
-    print(srcWords)
+    print(entry[1] + " : " + str(srcWords))
     # track letters left in each source word
     letters = [0,0,0,0,0]
     for i in range(5):
-        letters[i] = len(srcWords[i])
-    print(letters)
+        letters[i] = len(srcWords[i])-1
     # track current focus
-    lang = 0 # 'c' 's' 'h' 'a' 'r'
+    lang = 0 # 0=c, 1=s, 2=h, 3=a, 4=r
+    # randomly get letters from source words
     while (all(letters[i] > 0 for i in range(len(letters)))):
-        outputWord += srcWords[lang]
+        letterToAdd = srcWords[lang][len(srcWords[lang])-letters[lang]-1]
+        outputWord += letterToAdd
         letters[lang] -= 1
-        lang = randint(0,9)
+        lang = randint(0,4)
     return outputWord
 
+
+
 data = getData()
+
 for i in range(len(data)):
-    print(generateWord(data[i]))
-for i in range(len(data)):
-    print(getError(data[i]))
+    newWord = generateWord(data[i])
+    newWords.append(newWord)
+    output.append(newWord + ',' + data[i].split(',',1)[1][:-1])
+
+print(output)
+
+for i in range(len(newWords)):
+    print(str(getError(output[i])))
