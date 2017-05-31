e95ed78f9640121a671b86ff5a0d6635646c10f0
hchiam <hchiam@gmail.com>
2017-05-30 20:20:23 -0400

    comment out genetic algo for normal use

--- cognateLanguage_CreatingList.py
+++ cognateLanguage_CreatingList.py
@@ -307,18 +307,19 @@ for line in data:
     #     #newWord = createWord() # here is the major function call!
     #     #with open(outputFilename,'a') as f2:
     #         #f2.write(newWord + ',' + originalWords['Eng'] + ',' + originalWords['Chi'] + ',' + originalWords['Spa'] + ',' + #originalWords['Hin'] + ',' + originalWords['Ara'] + ',' + originalWords['Rus'] + ',\n')
-    # if words['Eng'] != 'Eng':
-    #     newWord = createWord_Alternate() # here is the major function call!
-    #     with open(outputFilename,'a') as f2:
-    #         f2.write(newWord + ',' + originalWords['Eng'] + ',' + originalWords['Chi'] + ',' + originalWords['Spa'] + ',' + originalWords['Hin'] + ',' + originalWords['Ara'] + ',' + originalWords['Rus'] + ',\n')
+    if words['Eng'] != 'Eng':
+        newWord = createWord_Alternate() # here is the major function call!
+        with open(outputFilename,'a') as f2:
+            f2.write(newWord + ',' + originalWords['Eng'] + ',' + originalWords['Chi'] + ',' + originalWords['Spa'] + ',' + originalWords['Hin'] + ',' + originalWords['Ara'] + ',' + originalWords['Rus'] + ',\n')
     # #if words['Eng'] != 'Eng':
     #     #newWord = createWord_DummyTest() # here is the major function call!
     #     #with open(outputFilename,'a') as f2:
     #         #f2.write(newWord + ',' + originalWords['Eng'] + ',' + originalWords['Chi'] + ',' + originalWords['Spa'] + ',' + #originalWords['Hin'] + ',' + originalWords['Ara'] + ',' + originalWords['Rus'] + ',\n')
-    if words['Eng'] != 'Eng':
-        newWord = createWord_GeneticAlgo(line) # here is the major function call!
-        with open(outputFilename,'a') as f2:
-            f2.write(newWord + ',' + originalWords['Eng'] + ',' + originalWords['Chi'] + ',' + originalWords['Spa'] + ',' + originalWords['Hin'] + ',' + originalWords['Ara'] + ',' + originalWords['Rus'] + ',\n')
+    
+    # if words['Eng'] != 'Eng':
+    #     newWord = createWord_GeneticAlgo(line) # here is the major function call!
+    #     with open(outputFilename,'a') as f2:
+    #         f2.write(newWord + ',' + originalWords['Eng'] + ',' + originalWords['Chi'] + ',' + originalWords['Spa'] + ',' + originalWords['Hin'] + ',' + originalWords['Ara'] + ',' + originalWords['Rus'] + ',\n')
 
 with open(outputFilename,'a') as f2:
     f2.write('____________________\n')
