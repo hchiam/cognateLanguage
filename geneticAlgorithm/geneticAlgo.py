d6d17fd20a888ec903033cf24de77f228de39aab
hchiam <hchiam@gmail.com>
2017-06-05 00:54:17 -0400

    close #5: favour 1st sylls (allos), streamline evaluate()

--- geneticAlgorithm/geneticAlgo.py
+++ geneticAlgorithm/geneticAlgo.py
@@ -2,6 +2,7 @@ from random import randint
 from operator import itemgetter
 import ast # to convert string of list to actual list
 import collections
+import re
 
 #------------------------
 # shared variables:
@@ -48,127 +49,6 @@ if debugOn:
 # functions:
 #------------------------
 
-def respellWithInitialVowelAndConsonants(word):
-    for char in word[1:]:
-        if char in 'aeiou':
-            word = word[0] + word[1:].replace(char,'')
-    return word
-
-
-def respellWithAllophones(word):
-    for char in word:
-        for allo in allophones:
-            if char in allo:
-                word = word.replace(char,allophones[allo])
-    return word
-
-
-def combineOverlappingWords(shortList):
-    for language in shortList:
-        for otherlanguage in shortList:
-            if language != otherlanguage and language != 'Eng' and otherlanguage != 'Eng':
-                a = shortList[language]
-                b = shortList[otherlanguage]
-                for i in range(1, len(b)):
-                    if a.endswith(b[:i]):
-                        shortList[otherlanguage] = ''
-                        shortList[language] = a+b[i:]
-    return shortList
-
-
-def evaluateScore_AlloWithVowels(word,originalWords):
-    score = 0
-    scoreLangs = [0] * len(originalWords)
-    
-    leastEfficientWord = ''.join(originalWords)
-    
-    # ABZDAVG allo w/ vowels
-    
-    alloWithVowels = respellWithAllophones(word)
-    #print 'Allophone Form of Word, with Vowels: ', alloWithVowels
-    
-    alloOriginalWords = list(originalWords) # careful with creating references that overwrite!
-    
-    for index, srcWord in enumerate(alloOriginalWords):
-        alloOriginalWords[index] = respellWithAllophones(srcWord)
-    
-    #print alloOriginalWords
-
-    # get preliminary scores for each language:
-    for lang, srcWordAllo in enumerate(alloOriginalWords):
-        for i in range(len(srcWordAllo)):
-            head = srcWordAllo[:i]
-            if head in respellWithAllophones(word):
-                # add to score per matching letter of word:
-                scoreLangs[lang] += 1
-    
-    # adjust language scores by number of characters in original words:
-    for lang, srcWordAllo in enumerate(alloOriginalWords):
-        scoreLangs[lang] -= len(srcWordAllo)
-
-    # language scores are weighted in reverse order
-    scoreLangs.reverse()
-
-    for wt, lang in enumerate(scoreLangs):
-        score += lang + lang * ((wt+1)/10.0) # make weightings like these to make gradient of influence:  0.1, 0.2, 0.3, 0.4, 0.5
-    #print 'language score contribution: ', score
-    
-    # get preliminary score for word length:
-    scoreLen = (len(leastEfficientWord) - len(word)) # score increases with shorter word
-    scoreLen *= 1.1 # this is the weighting for length score
-    #print 'word length contribution', scoreLen
-    score += scoreLen
-
-    return round(score,2)
-
-
-def evaluateScore_ConsonantsInOrder(word,originalWords):
-    score = 0
-    scoreLangs = [0] * len(originalWords)
-    
-    leastEfficientWord = ''.join(originalWords)
-    
-    alloConsonants = list(originalWords) # careful with creating references that overwrite!
-    alloOfNewWord = respellWithAllophones(word).replace('a','').replace('e','').replace('i','').replace('o','').replace('u','')
-    
-    #print alloOfNewWord
-    
-    for index, srcWord in enumerate(alloConsonants):
-        alloConsonants[index] = respellWithAllophones(srcWord).replace('a','').replace('e','').replace('i','').replace('o','').replace('u','')
-    
-    #print alloConsonants
-    
-    # BZDVG
-    
-    # go through each language's test pattern:
-    for lang, testPattern in enumerate(alloConsonants):
-        currentLetterPos = 0
-        # go through as many letters of that test pattern as possible:
-        for i in range(1,len(testPattern)):
-            # if that letter is found in new word then update current letter position (= index+1 since list indices start at 0):
-            if testPattern[i] in alloOfNewWord:
-                #print testPattern[i]
-                currentLetterPos = i+1
-        # use full word length - the current letter into the test pattern as the score for that language
-        scoreLangs[lang] = currentLetterPos - len(originalWords[lang])
-        currentLetterPos = 0
-        #print scoreLangs
-
-    # language scores are weighted in reverse order
-    scoreLangs.reverse()
-
-    for wt, lang in enumerate(scoreLangs):
-        score += lang + lang * ((wt+1)/10.0) # make weightings like these to make gradient of influence:  0.1, 0.2, 0.3, 0.4, 0.5
-
-    # get preliminary score for word length:
-    scoreLen = (len(leastEfficientWord) - len(word)) # score increases with shorter word
-    scoreLen *= 1.1 # this is the weighting for length score
-    #print 'word length contribution', scoreLen
-    score += scoreLen
-    
-    return round(score,2)
-
-
 def evaluateScore_LettersFromEachSource(word,originalWords):
     score = 0
     consonantsAlreadyUsed = []
@@ -179,6 +59,30 @@ def evaluateScore_LettersFromEachSource(word,originalWords):
             # encourage using words with letters found in all source words:
             for srcWord in originalWords:
                 score += 1 if letter in srcWord else 0
+    return score
+
+
+def getFirstSyllable(string):
+    syll = re.search(r'[^aeiou]+[aeiou][^aeiou]?',string)
+    if syll:
+        return syll.group() # CV, CVC, CCVC, ...
+    return string # in case word only has vowels (V or VV...)
+
+
+def respellWithAllophones(word):
+    for char in word:
+        for allo in allophones:
+            if char in allo:
+                word = word.replace(char,allophones[allo])
+    return word
+
+
+def evaluateScore_UsesFirstSyllablesAllophones(newWord, originalWords):
+    score = 0
+    srcSyllables = [getFirstSyllable(srcWord) for srcWord in originalWords if srcWord!=None]
+    for syllable in srcSyllables:
+        if respellWithAllophones(syllable) in respellWithAllophones(newWord):
+            score += len(syllable)*2 # *2 to make it more worth it
     return score
 
 
@@ -197,22 +101,21 @@ def penalizeConsonantClusters(word):
     return score
 
 
+def penalizeLength(newWord):
+    return -len(newWord)
+
+
 def evaluate(line):
     newWord = line.split(',')[0]
     originalWords = line.split(',')[2:]
     score = 0
-    
-    # evaluators with different weighting priorities for different source languages:
-    score += evaluateScore_AlloWithVowels(newWord, originalWords)
-    score += evaluateScore_ConsonantsInOrder(newWord, originalWords)
-    
     # encourage using letters from ALL src words, but avoid repeating letters like in "mmmmmmommmmmmm":
     score += evaluateScore_LettersFromEachSource(newWord, originalWords)
-    
+    score += evaluateScore_UsesFirstSyllablesAllophones(newWord,originalWords)
     # avoid consonant clusters like in "htkyowaz" or "kdyspgunwa"
     score += penalizeConsonantClusters(newWord)
-    
-    return round(score, 2)
+    score += penalizeLength(newWord)
+    return score
 
 
 def getSourceWords(data):
@@ -292,7 +195,7 @@ def getEntryIdentifier(entry):
 
 
 def getEntryScore(entry):
-    return str(entry).split(', ',1)[0][1:] # [1:] to remove initial '['
+    return int(str(entry).split(', ',1)[0][1:]) # [1:] to remove initial '['
 
 
 def printDebug(*args):
@@ -337,28 +240,28 @@ def createWord(inputLineEntry):
         individual = [score, entry, instructions]
         population.append(individual)
     
-    # # randomize whether initialization includes previous best-scorer in this session's population
-    # # (later will still compare to it anyways to check for improved score)
-    # cointoss = randint(0,1)
-    # if cointoss == 0:
-    #     creatingFromScratch = True
-    # elif cointoss == 1:
-    # make use of preexisting best-scorer saved externally
-    scorersFile = 'best-scorers.txt'
-    scorers = []
-    creatingFromScratch = False
-    with open(scorersFile,'r') as f:
-        scorers = f.read().splitlines()
-    if scorers != []:
-        for scorer in scorers:
-            prevScorer_id = getEntryIdentifier(scorer)
-            if prevScorer_id == getEntryIdentifier(population[0]):
-                prevBestScore = float(scorer.split(', ')[0].replace('[',''))
-                prevBestEntry = scorer.split(', ')[1].replace('\'','')
-                prevBestInstruction = ast.literal_eval(scorer.split(', ',2)[2][:-1]) # [:-1] to remove final ']'
-                prevBest = [prevBestScore,prevBestEntry,prevBestInstruction]
-                # include preexisting best-scorer saved externally
-                population.append(prevBest)
+    # randomize whether initialization includes previous best-scorer in this session's population
+    # (later will still compare to it anyways to check for improved score)
+    cointoss = randint(0,1)
+    if cointoss == 0:
+        creatingFromScratch = True
+    elif cointoss == 1:
+        # make use of preexisting best-scorer saved externally
+        scorersFile = 'best-scorers.txt'
+        scorers = []
+        creatingFromScratch = False
+        with open(scorersFile,'r') as f:
+            scorers = f.read().splitlines()
+        if scorers != []:
+            for scorer in scorers:
+                prevScorer_id = getEntryIdentifier(scorer)
+                if prevScorer_id == getEntryIdentifier(population[0]):
+                    prevBestScore = int(float(scorer.split(', ')[0].replace('[','')))
+                    prevBestEntry = scorer.split(', ')[1].replace('\'','')
+                    prevBestInstruction = ast.literal_eval(scorer.split(', ',2)[2][:-1]) # [:-1] to remove final ']'
+                    prevBest = [prevBestScore,prevBestEntry,prevBestInstruction]
+                    # include preexisting best-scorer saved externally
+                    population.append(prevBest)
     
     # starting "from scratch"? allow more generations before comparing with best scorer
     adjustForFromScratch = 1
@@ -514,7 +417,7 @@ def createWord(inputLineEntry):
                         print(bestSoFar)
                         countNew += 1
                         print('NEW WORDS:',countNew)
-                        scoreImprovements += float(bestSoFar_scr) - float(prevScorer_scr)
+                        scoreImprovements += bestSoFar_scr - prevScorer_scr
                         print('SCORE IMPROVEMENT SUM:',scoreImprovements)
                     else:
                         newScorers.append(scorer)
