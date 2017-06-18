974166991c9cfaf0bfaabaa775b47cdfdb852e8f
hchiam <hchiam@gmail.com>
2017-06-18 18:26:34 -0400

    print()

--- dictAsFile_wrapper.py
+++ dictAsFile_wrapper.py
@@ -26,5 +26,5 @@ if __name__ == '__main__':
     fileName = 'myfile.pkl'
     writeDictToFile(myDict,fileName)
     dictBack = readFileToDict(fileName)
-    print myDict
-    print dictBack
+    print (myDict)
+    print (dictBack)
