ac9741f6b5cb69a4c18b2b7ad0481cb372f539ed
hchiam <hchiam@gmail.com>
2017-10-31 21:31:11 -0400

    order for consistency

--- findCollisions.py
+++ findCollisions.py
@@ -22,21 +22,6 @@ def countCollisionsInFile(filename,cv=False,cvc=False):
 def countCollisionsInList(entries):
     return countCollisions(entries)
 
-def justTwoInitSylls_CVC(word):
-    beforeThisIndex = 0
-    afterThisIndex = 0
-    for vowel1 in word:
-        if vowel1 in 'aeiou':
-            afterThisIndex = word.index(vowel1)
-            break
-    for vowel2 in word[afterThisIndex+1:]:
-        if vowel2 in 'aeiou':
-            beforeThisIndex = word[afterThisIndex+1:].index(vowel2)+1 + afterThisIndex+1
-            break
-    if beforeThisIndex!=0:
-        word = word[:beforeThisIndex+1]
-    return word
-
 def justTwoInitSylls_CV(word):
     beforeThisIndex = 0
     afterThisIndex = 0
@@ -50,6 +35,21 @@ def justTwoInitSylls_CV(word):
             break
     if beforeThisIndex!=0:
         word = word[:beforeThisIndex]
+    return word
+
+def justTwoInitSylls_CVC(word):
+    beforeThisIndex = 0
+    afterThisIndex = 0
+    for vowel1 in word:
+        if vowel1 in 'aeiou':
+            afterThisIndex = word.index(vowel1)
+            break
+    for vowel2 in word[afterThisIndex+1:]:
+        if vowel2 in 'aeiou':
+            beforeThisIndex = word[afterThisIndex+1:].index(vowel2)+1 + afterThisIndex+1
+            break
+    if beforeThisIndex!=0:
+        word = word[:beforeThisIndex+1]
     return word
 
 if __name__ == '__main__': # if running this .py file directly
